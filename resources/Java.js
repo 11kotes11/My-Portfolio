2bd655854848ab744a1dffd1bc217e518af422bc
Dakota Hatch <hdakota2018@gmail.com>
2023-07-07 11:42:22 -0400

    Add Heroi Background

--- resources/Java.js
+++ resources/Java.js
@@ -1,8 +1,6 @@
 const skill = document.getElementsByClassName('skill');
 const favicon = document.getElementsByClassName('favicon');
 
-skill.addEventListener("click", scale);
-
 function scale() {
   favicon.style.width= "70px";
 }
